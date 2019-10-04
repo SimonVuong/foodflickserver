b715c650541b57ee214a21cad05fafd1eec6c77b
Alvin Chau <alvinchau2013@gmail.com>
2019-10-03 21:04:13 -0400

    Removed analytics folder and uninstalled amplitude

--- client/src/analytics/AnalyticsService.ts
+++
@@ -1,7 +0,0 @@
-import { activeConfig } from '../config';
-import EVENTS from './events';
-import amplitude from 'amplitude-js';
-
-class AnalyticsService {
-
-}
--- client/src/analytics/events.ts
+++
@@ -1,9 +0,0 @@
-const EVENTS = {
-    SEARCH_BAR: "SEARCH_BAR",
-    SIGN_IN: "SIGN_IN",
-    SIGN_OUT: "SIGN_OUT",
-    ABOUT_FOODFLICK: "ABOUT_FOODFLICK",
-
-}
-
-export default EVENTS;
